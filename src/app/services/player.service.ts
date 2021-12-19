3ff4897450d036ead8e254d7c28e49efe4d5d01d
xtasylabyrinth <xtasylabyrinth@gmail.com>
2021-12-20 04:55:21 +0800

    Update player service

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { PlayerBalance } from '../interfaces/player-balance';
+import { PlayerDetails } from '../interfaces/player-details';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  fetchPlayerDetails(playerName: string) {
+    return this.http.get<PlayerDetails>(`https://api2.splinterlands.com/players/details?name=${playerName}`);
+  }
+
+  fetchPlayerBalances(playerName: string) {
+    return this.http.get<PlayerBalance[]>(`https://api2.splinterlands.com/players/balances?username=${playerName}`);
+  }
 }
