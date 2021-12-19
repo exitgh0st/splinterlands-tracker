5e885604c8d0d5854b0e6ab2cdfd129d010c77a0
xtasylabyrinth <xtasylabyrinth@gmail.com>
2021-12-20 04:53:23 +0800

    Add player-details interface

---
+++ src/app/interfaces/player-details.ts
@@ -0,0 +1,10 @@
+export interface PlayerDetails {
+    name: string;
+    rating: number;
+    battles: number;
+    wins: number;
+    capture_rate: number;
+    last_reward_block: number;
+    collection_power: number;
+    league: number; 
+}
