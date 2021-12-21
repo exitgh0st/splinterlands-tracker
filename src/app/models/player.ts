86928d26ffc99636aa0c47142177df81b7b374db
xtasylabyrinth <xtasylabyrinth@gmail.com>
2021-12-21 15:59:32 +0800

    Create Player class

---
+++ src/app/models/player.ts
@@ -0,0 +1,15 @@
+export class Player {
+  name?: string;
+  rating?: number;
+  battles?: number;
+  wins?: number;
+  winRate?: number;
+  captureRate?: number;
+  collectionPower?: number;
+  league?: number;
+  rank?: string;
+  dec?: number;
+  sps?: number;
+  stakedSps?: number;
+  credits?: number;
+}
