6f740005d064641fccf5b48fa517a327dcb472d7
xtasylabyrinth <xtasylabyrinth@gmail.com>
2021-12-21 15:53:44 +0800

    Rename ModalIdsEnum to ModalIds

--- src/app/contants/modal-classes.ts
+++ src/app/contants/modal-classes.ts
@@ -1,8 +1,8 @@
 import { AddAccountModalComponent } from "../components/add-account-modal/add-account-modal.component";
 import { ModalComponent } from "../components/modal/modal.component";
-import { ModalIdsEnum } from "../enums/modal-ids-enum";
+import { ModalIds } from "../enums/modal-ids";
 
 export const ModalClasses = {
-  [ModalIdsEnum.MODAL_DEFAULT_ID] : ModalComponent,
-  [ModalIdsEnum.ADD_ACCOUNT_MODAL_ID]: AddAccountModalComponent
+  [ModalIds.MODAL_DEFAULT_ID] : ModalComponent,
+  [ModalIds.ADD_ACCOUNT_MODAL_ID]: AddAccountModalComponent
 };
