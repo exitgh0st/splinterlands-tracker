2cbe20182587d6798f349a98f4ca14eb5c160d9b
xtasylabyrinth <xtasylabyrinth@gmail.com>
2021-12-21 15:59:49 +0800

    Update CurrencyService

--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { LocalStorageKeys } from '../contants/local-storage-keys';
+import { Currency } from '../enums/currency';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrencyService {
 
-  constructor() { }
+  nativeCurrency: Currency = Currency.PHP;
+
+  constructor() {
+    const nativeCurrencyString = localStorage.getItem(LocalStorageKeys.NATIVE_CURRENCY);
+
+    if (nativeCurrencyString) {
+      this.nativeCurrency = Currency[<Currency>nativeCurrencyString];
+    }else {
+      localStorage.setItem(LocalStorageKeys.NATIVE_CURRENCY, this.nativeCurrency);
+    }
+  }
+
+  getNativeCurrency() {
+    return this.nativeCurrency;
+  }
+
+
 }
