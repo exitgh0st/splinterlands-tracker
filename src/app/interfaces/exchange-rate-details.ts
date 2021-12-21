231d18a9a939a4148dd916a52a4b97a918fa0337
xtasylabyrinth <xtasylabyrinth@gmail.com>
2021-12-21 15:57:09 +0800

    Update exchange-rate-details.ts

--- src/app/interfaces/exchange-rate-details.ts
+++ src/app/interfaces/exchange-rate-details.ts
@@ -1,8 +1,8 @@
 import { Currency } from "../enums/currency";
 
 export interface ExchangeRateDetails {
-  time_last_updated?: number;
-  rates?: {
+  time_last_updated: number;
+  rates: {
       [key in Currency]: number;
   };
 }
