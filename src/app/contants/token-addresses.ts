8429a5e1a178c27525e37a8554d548aa6b650850
xtasylabyrinth <xtasylabyrinth@gmail.com>
2021-12-21 15:54:02 +0800

    Create TokenAddresses class

---
+++ src/app/contants/token-addresses.ts
@@ -0,0 +1,4 @@
+export class TokenAddresses {
+  public static readonly DEC_TOKEN_ADDRESS = "0xe9d7023f2132d55cbd4ee1f78273cb7a3e74f10a";
+  public static readonly SPS_TOKEN_ADDRESS = "0x1633b7157e7638c4d6593436111bf125ee74703f";
+}
