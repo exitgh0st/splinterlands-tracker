58e91be5ab668c096b53ba37150ed27d21a06fd1
xtasylabyrinth <xtasylabyrinth@gmail.com>
2022-01-07 08:05:04 +0800

    Add error to player details interface

--- src/app/interfaces/player-details.ts
+++ src/app/interfaces/player-details.ts
@@ -1,10 +1,11 @@
 export interface PlayerDetails {
-    name: string;
-    rating: number;
-    battles: number;
-    wins: number;
-    capture_rate: number;
-    last_reward_block: number;
-    collection_power: number;
-    league: number; 
+    name?: string;
+    rating?: number;
+    battles?: number;
+    wins?: number;
+    capture_rate?: number;
+    last_reward_block?: number;
+    collection_power?: number;
+    league?: number;
+    error?: string;
 }
