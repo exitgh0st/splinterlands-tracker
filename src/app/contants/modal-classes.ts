253009b917349dc11fc2013b2b1697e408909e7d
xtasylabyrinth <xtasylabyrinth@gmail.com>
2022-01-07 08:07:14 +0800

    Add constants for settings modal

--- src/app/contants/modal-classes.ts
+++ src/app/contants/modal-classes.ts
@@ -1,8 +1,10 @@
 import { AddAccountModalComponent } from "../components/add-account-modal/add-account-modal.component";
 import { ModalComponent } from "../components/modal/modal.component";
+import { SettingsModalComponent } from "../components/settings-modal/settings-modal.component";
 import { ModalIds } from "../enums/modal-ids";
 
 export const ModalClasses = {
   [ModalIds.MODAL_DEFAULT_ID] : ModalComponent,
-  [ModalIds.ADD_ACCOUNT_MODAL_ID]: AddAccountModalComponent
+  [ModalIds.ADD_ACCOUNT_MODAL_ID]: AddAccountModalComponent,
+  [ModalIds.SETTINGS_MODAL_ID]: SettingsModalComponent
 };
