de18ad3a468c3bd439f3505e14e36ae103688ced
xtasylabyrinth <xtasylabyrinth@gmail.com>
2022-01-26 09:31:12 +0800

    Rename contants folder to constants

--- src/app/constants/modal-classes.ts
+++ src/app/constants/modal-classes.ts
@@ -1,4 +1,5 @@
 import { AddAccountModalComponent } from "../components/add-account-modal/add-account-modal.component";
+import { ErrorModalComponent } from "../components/error-modal/error-modal.component";
 import { ModalComponent } from "../components/modal/modal.component";
 import { SettingsModalComponent } from "../components/settings-modal/settings-modal.component";
 import { ModalIds } from "../enums/modal-ids";
@@ -6,5 +7,6 @@ import { ModalIds } from "../enums/modal-ids";
 export const ModalClasses = {
   [ModalIds.MODAL_DEFAULT_ID] : ModalComponent,
   [ModalIds.ADD_ACCOUNT_MODAL_ID]: AddAccountModalComponent,
-  [ModalIds.SETTINGS_MODAL_ID]: SettingsModalComponent
+  [ModalIds.SETTINGS_MODAL_ID]: SettingsModalComponent,
+  [ModalIds.ERROR_MODAL_ID]: ErrorModalComponent
 };
